ac69c81e8cfd1e5b821477516e0abeefdba0fc4c
Biome.io <fahadbajwa@biome.io>
2025-04-29 12:37:41 +0500

    Removed provided root access

--- src/app/messaging-container/services/message.service.ts
+++ src/app/messaging-container/services/message.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root' })
+// @Injectable({ providedIn: 'root' })
+
+@Injectable()   // No providedIn here
+
 export class SharedService {
   message = '';   
 }
